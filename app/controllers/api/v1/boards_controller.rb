a31aeab8043bf215113ebe3612717c2908f76a61
Matt McAlister <matt.mcalister93@gmail.com>
2018-01-23 16:53:05 -0500

    organized filestructure

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+    render json: @boards
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    render json: @board
+  end
 end
